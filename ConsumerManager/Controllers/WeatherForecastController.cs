d75e70d0031986b60e578d99f1c5d5facf94af75
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-09-16 06:28:11 +0100

    WP-001: Basic unit test for WeatherForecastController::Get
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- ConsumerManager/Controllers/WeatherForecastController.cs
+++ ConsumerManager/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace ConsumerManager.Controllers
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+      _logger.LogInformation("Getting five weather forecasts");
       return Enumerable.Range(1, 5).Select(index => new WeatherForecast
       {
         Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
