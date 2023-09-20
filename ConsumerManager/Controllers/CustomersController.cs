cc529c671da1dda4c4cfed39901eed97b9319b45
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-09-20 21:37:58 +0100

    WP-002: Adding validation check for pre-existing email or phone
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- ConsumerManager/Controllers/CustomersController.cs
+++ ConsumerManager/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using ConsumerManager.Entities.Database;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
 namespace ConsumerManager.Controllers
@@ -39,19 +40,35 @@ namespace ConsumerManager.Controllers
       return customer;
     }
     [HttpPost]
-    public async Task<ActionResult<Customer>> Create(CreateCustomerContract contract)
+    public async Task<ActionResult<Customer>> Create([FromBody] CreateCustomerContract contract)
     {
-      // model.AddAsync(contract);
+      logger.LogInformation("Trying to add a new customer");
+      if (contract is null)
+      {
+        return BadRequest("You need to provide data for new customer.");
+      }
+
+      var existing = await model.Customers.FirstOrDefaultAsync(
+        customer => customer.Email == contract.Email.ToLower()
+        || customer.Phone == contract.Phone
+      );
+
+      if (existing is not null)
+      {
+        return BadRequest($"A customer with the email '{contract.Email}' and/or phone '{contract.Phone}' already exists.");
+      }
+
       var customer = new Customer
       {
         Title = contract.Title,
         Forename = contract.Forename,
         Surename = contract.Surename,
-        Email = contract.Email,
+        Email = contract.Email.ToLower(),
         Phone = contract.Phone,
         CreatedAt = DateTime.UtcNow,
         LastUpdatedAt = DateTime.UtcNow,
       };
+
       await model.AddAsync(customer);
       await model.SaveChangesAsync();
       logger.LogInformation($"Created new customer with Id = {customer.Id}");
